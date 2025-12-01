e5fa71dd1f55e3e4ab53893136a420eebeffe63f
Titanlert <125745209+Titanlert@users.noreply.github.com>
2025-12-01 14:53:37 -0500

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -3,15 +3,13 @@ import streamlit as st
 st.title("Web Development Lab03")
 
 st.header("CS 1301")
-st.subheader("Team 1, Web Development - Section A")
+st.subheader("Team 72, Web Development - Section A")
 st.subheader("Gustavo Zermeno, Titanon Lertussawawiwat, William Nathaniel Faustino Pugeda")
 
 st.write("""
 Welcome to our Streamlit Web Development Lab03 app. Use the sidebar to open the pages.
 
 1. **Phase 2 API Page**: Shows simple weather data from a web API with a chart.
-2. **Phase 3 Gemini Page**: Uses the same weather data and Google Gemini to write a short text.
-3. **Phase 4 Chat Page**: Chatbot that answers questions about the weather data.
-4. **Extra Content**: Any other simple things we add later.
-""")
+2. **Phase 3 Gemini Page**: Uses the same weather data and Google Gemini to write either a short summary or with extra details.
+3. **Phase 4 Chat Page**: Chatbot that answers questions about the weather data, including advice on clothes or activities to do in the type of weather.
 
